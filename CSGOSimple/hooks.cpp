c0ca0f6037715fede4880971e28ff0d466b030e2
roccoishot <theaceattorney05@gmail.com>
2021-09-18 00:43:10 -0400

    Update hooks.cpp

--- CSGOSimple/hooks.cpp
+++ CSGOSimple/hooks.cpp
@@ -206,6 +206,9 @@ namespace Hooks {
 		static auto r_modelAmbientMin = g_CVar->FindVar("r_modelAmbientMin");
 		if (g_Options.amibence && r_modelAmbientMin->GetFloat() != g_Options.amibence * 0.05f) //-V550
 			r_modelAmbientMin->SetValue(g_Options.amibence * 0.05f);
+		else if ((g_Options.amibence < 2.f) && r_modelAmbientMin->GetFloat())
+			r_modelAmbientMin->SetValue(0.0f);
+
 
 		//Shot Info
 		if (g_Options.shotinfo == false) {
